26ec1f15e1ec5cbdacde8ebce9b7a954a71d5e78
Sam7144 <79979572+Sam7144@users.noreply.github.com>
2023-11-11 11:20:54 +0300

    firebase

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import {getFirestore} from "firebase/firestore"
 import {getFunctions} from "firebase/functions"
 import{getStorage} from "firebase/storage"
 const firebaseConfig = {
-  apiKey: "AIzaSyAxblsazPzUNtn2vWLzmZ7VafBuwJx6SGo",
-  authDomain: "cloud-storage-daed7.firebaseapp.com",
-  projectId: "cloud-storage-daed7",
-  storageBucket: "cloud-storage-daed7.appspot.com",
-  messagingSenderId: "413290331963",
-  appId: "1:413290331963:web:6d2e2e0f4475b142f8c11e",
-  measurementId: "G-3VLJ8QS4L2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
